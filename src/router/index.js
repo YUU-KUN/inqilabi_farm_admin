344de7755226649b7cabd5a941919241b3d4dfa8
YUU-KUN <indra_wahyu_27rpl@student.smktelkom-mlg.sch.id>
2021-07-03 07:25:12 +0700

    routing

--- src/router/index.js
+++ src/router/index.js
@@ -1,12 +1,113 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
+import store from '../store'
+//layouts
+import Header from '../views/layouts/Header.vue'
+import Footer from '../views/layouts/Footer.vue'
+
+// auth
+import Login from '../views/auth/Login.vue'
+import Register from '../views/auth/Register.vue'
+
+// pages
 import Home from '../views/Home.vue'
+import Dashboard from '../views/Dashboard.vue'
+import Pembayaran from '../views/Pembayaran.vue'
+import Laporan from '../views/Laporan.vue'
+import LaporanDetail from '../views/Laporan Detail.vue'
+import LaporanCreate from '../views/Laporan Create.vue'
+import LaporanEdit from '../views/Laporan Edit.vue'
+import Pengiriman from '../views/Pengiriman.vue'
+import Sertifikat from '../views/Sertifikat.vue'
+import SertifikatCreate from '../views/Sertifikat Create.vue'
 
 Vue.use(VueRouter)
 
 const routes = [
   {
+    path: '/create/:id',
+    name: 'Create Certificate',
+    components: {default: SertifikatCreate, footer: Footer, header: Header},
+    meta: { 
+      requiresAuth: true
+    }
+  },
+  {
+    path: '/sertifikat',
+    name: 'Sertifikat',
+    components: {default: Sertifikat, footer: Footer, header: Header},
+    meta: { 
+      requiresAuth: true
+    }
+  },
+  {
+    path: '/laporan/pengiriman/:id_pembayaran',
+    name: 'Pengiriman',
+    components: {default: Pengiriman, footer: Footer, header: Header},
+    meta: { 
+      requiresAuth: true
+    }
+  },
+  {
+    path: '/laporan/:id_pembayaran/:id_laporan',
+    name: 'reportEdit',
+    components: {default: LaporanEdit, footer: Footer, header: Header},
+    meta: {
+      requiresAuth: true
+    }
+  },
+  {
+    path: '/laporan/create',
+    name: 'reportCreate',
+    components: {default: LaporanCreate, footer: Footer, header: Header},
+    meta: {
+      requiresAuth: true
+    }
+  },
+  {
+    path: '/laporan/:id_pembayaran',
+    name: 'reportDetail',
+    components: {default: LaporanDetail, footer: Footer, header: Header},
+    meta: {
+      requiresAuth: true
+    }
+  },
+  {
+    path: '/laporan',
+    name: 'Laporan',
+    components: {default: Laporan, footer: Footer, header: Header},
+    meta: {
+      requiresAuth: true
+    }
+  },
+  {
+    path: '/pembayaran',
+    name: 'Pembayaran',
+    components: {default: Pembayaran, footer: Footer, header: Header},
+    meta: {
+      requiresAuth: true
+    }
+  },
+  {
     path: '/',
+    name: 'Dashboard',
+    components: {default: Dashboard, footer: Footer, header: Header},
+    meta: {
+      requiresAuth: true
+    }
+  },
+  {
+    path: '/register',
+    name: 'Register',
+    components: {default: Register, footer: Footer, header: Header}
+  },
+  {
+    path: '/login',
+    name: 'Login',
+    components: {default: Login, footer: Footer, header: Header}
+  },
+  {
+    path: '/home',
     name: 'Home',
     component: Home
   },
@@ -24,6 +125,18 @@ const router = new VueRouter({
   mode: 'history',
   base: process.env.BASE_URL,
   routes
+})
+
+router.beforeEach((to, from, next) => {
+  if(to.matched.some(record => record.meta.requiresAuth)) {
+    if (store.getters.isLoggedIn) {
+      next()
+      return
+    }
+    next('/login') 
+  } else {
+    next() 
+  }
 })
 
 export default router
